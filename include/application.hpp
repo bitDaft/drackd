8d5010b303ad25f3670abaf679e56e8a493d1edc
tausif <ajaxhis@tutanota.com>
2024-12-15 22:16:04 +0530

    added window creation and fullscreen support with grabbing

--- include/application.hpp
+++ include/application.hpp
@@ -4,6 +4,8 @@
 #include <string>
 #include <vector>
 
+#include <X11/Xlib.h>
+
 #include "appConfig.hpp"
 
 class Application
@@ -12,12 +14,26 @@ public:
   Application(int argc, char *argv[]);
   ~Application();
 
+  void run();
+
 private:
   std::string stdinData;
+
   AppConfig appConfig;
+
+  Display *display;
+  Window root, window;
+  int screen;
+  Atom clip, utf8;
+  GC gc;
+  Drawable pixmap;
 
 private:
   void createAppWindow();
+  void handleWindowEvents();
+  void render();
+  void grabKeyboard();
+  void makeFullscreen();
   void updateSizes(int windowWidth, int windowHeight);
 };
 
--- src/application.cpp
+++ src/application.cpp
@@ -3,28 +3,224 @@
 #include <getopt.h>
 #include <unistd.h>
 #include <sys/select.h>
+#include <X11/keysym.h>
+#include <X11/Xutil.h>
+#include <X11/Xatom.h>
 
 #include "application.hpp"
-#include "usage.hpp"
+#include "utils.hpp"
 
 // forward declaring needed function
-bool parseArguments(AppConfig &appConfig, int argc, char **argv);
+bool parseArguments(AppConfig &appConfig, int argc, char **argv, std::string &errorMessage);
 void checkAndReadStdin(std::string &input);
 
 Application::Application(int argc, char *argv[])
 {
-  if (argc == 1 || !parseArguments(appConfig, argc, argv))
-    exitWithUsage();
+  if (!setlocale(LC_CTYPE, "") || !XSupportsLocale())
+    std::cerr << "warning: no locale support\n";
+
+  std::string errorMsg;
+  if (argc == 1 || !parseArguments(appConfig, argc, argv, errorMsg))
+    printAndDie(true, errorMsg);
 
   checkAndReadStdin(stdinData);
 }
 
 Application::~Application()
 {
+  XDestroyWindow(display, window);
+  XCloseDisplay(display);
+}
+
+void Application::run()
+{
+  createAppWindow();
+  if (appConfig.grabKeyboard)
+    grabKeyboard();
+  handleWindowEvents();
 }
 
 void Application::createAppWindow()
 {
+  display = XOpenDisplay(NULL);
+  if (!display)
+    printAndDie(false, "Unable to open X display");
+
+  screen = DefaultScreen(display);
+  root = RootWindow(display, screen);
+
+  clip = XInternAtom(display, "CLIPBOARD", False);
+  utf8 = XInternAtom(display, "UTF8_STRING", False);
+
+  unsigned long black = BlackPixel(display, screen);
+  unsigned long white = WhitePixel(display, screen);
+
+  XSetWindowAttributes swa;
+  swa.override_redirect = False;
+  swa.background_pixel = black;
+  swa.event_mask = SubstructureRedirectMask | SubstructureNotifyMask | StructureNotifyMask | ExposureMask | KeyPressMask | VisibilityChangeMask | PropertyChangeMask;
+  window = XCreateWindow(display, root, 0, 0, appConfig.width, appConfig.height, 0,
+                         CopyFromParent, CopyFromParent, CopyFromParent,
+                         CWOverrideRedirect | CWBackPixel | CWEventMask, &swa);
+
+  Atom wmState = XInternAtom(display, "_NET_WM_STATE", False);
+  Atom fullscreenAtom = XInternAtom(display, "_NET_WM_STATE_FULLSCREEN", False);
+  // Atom wmAllowedActions = XInternAtom(display, "_NET_WM_ALLOWED_ACTIONS", False);
+  // Atom wmActionFullscreen = XInternAtom(display, "_NET_WM_ACTION_FULLSCREEN", False);
+  // Atom atoms[2] = {XInternAtom(display, "_NET_WM_STATE_FULLSCREEN", False), None};
+  // Atom wmActionFullscreen = XInternAtom(display, "_NET_WM_ACTION_FULLSCREEN", False);
+
+  // Disallow fullscreen toggling
+  XChangeProperty(display, window, wmState, XA_ATOM, 32,
+                  PropModeReplace, (unsigned char *)&fullscreenAtom, 1);
+  // XChangeProperty(display, window, wmAllowedActions, XA_ATOM, 32,
+  //                 PropModeReplace, (unsigned char *)&wmActionFullscreen, 0);
+
+  // window = XCreateSimpleWindow(display, root, 0, 0, appConfig.width, appConfig.height, 0, black, black);
+  XClassHint ch = {"drackd", "drackd"};
+  XStoreName(display, window, "drackd");
+  XSetClassHint(display, window, &ch);
+
+  XWindowAttributes wa;
+  if (!XGetWindowAttributes(display, window, &wa))
+    printAndDie("could not get created window attributes");
+
+  gc = XCreateGC(display, window, 0, NULL);
+  // pixmap = XCreatePixmap(display, root, wa.width, wa.height, DefaultDepth(display, screen));
+
+  // XSelectInput(display, window, StructureNotifyMask | ExposureMask | KeyPressMask | VisibilityChangeMask);
+  XMapWindow(display, window);
+
+  XSetForeground(display, gc, white);
+
+  XEvent e;
+  for (;;)
+  {
+    XNextEvent(display, &e);
+    if (e.type == MapNotify)
+      break;
+  }
+}
+
+void Application::handleWindowEvents()
+{
+  XEvent event;
+  while (true)
+  {
+    XNextEvent(display, &event);
+    if (event.type == KeyPress)
+    {
+      std::cout << "key pressed\n";
+      KeySym key = XLookupKeysym(&event.xkey, 0); // Get the key symbol
+      if (key == XK_Escape)
+      {        // Check if the Escape key was pressed
+        break; // Exit the loop
+      }
+    }
+    else if (event.type == ConfigureNotify && appConfig.fullscreen)
+    {
+      // Force fullscreen geometry
+      std::cout << "geochange\n";
+      // makeFullscreen();
+      // XMoveResizeWindow(display, window, 0, 0,
+      //                   DisplayWidth(display, screen),
+      //                   DisplayHeight(display, screen));
+    }
+    else if (event.type == PropertyNotify && appConfig.fullscreen)
+    {
+      // Force fullscreen geometry
+      std::cout << "PropertyNotify\n";
+      Atom wmState = XInternAtom(display, "_NET_WM_STATE", True);
+      Atom fullscreenAtom = XInternAtom(display, "_NET_WM_STATE_FULLSCREEN", True);
+      Atom type;
+      int format;
+      unsigned long length;
+      unsigned long after;
+      unsigned char *dp;
+      int status = XGetWindowProperty(display, window, wmState,
+                                      0L, (~0L), 0,
+                                      AnyPropertyType, &type, &format,
+                                      &length, &after, &dp);
+      if (status != Success)
+        continue;
+      int found = 0;
+      for (int i = 0; i < length; i++)
+      {
+        std::string atomName = XGetAtomName(display, ((Atom *)dp)[i]);
+        if (std::string("_NET_WM_STATE_FULLSCREEN") == atomName)
+        {
+          found = true;
+          break;
+        }
+      }
+      if (!found)
+      {
+        makeFullscreen();
+      }
+      XFree(dp);
+    }
+    else
+    {
+      // std::cout << "Unhandled event type: " << event.type << std::endl;
+    }
+    render();
+    XFlush(display);
+  }
+}
+
+void Application::grabKeyboard()
+{
+  struct timespec ts = {.tv_sec = 0, .tv_nsec = 1000000};
+  int i;
+
+  for (i = 0; i < 1000; i++)
+  {
+    if (XGrabKeyboard(display, DefaultRootWindow(display), True, GrabModeAsync,
+                      GrabModeAsync, CurrentTime) == GrabSuccess)
+      return;
+    nanosleep(&ts, NULL);
+  }
+  printAndDie(false, "Could not grab keyboard");
+}
+
+void Application::makeFullscreen()
+{
+  if (!appConfig.fullscreen)
+    return;
+  std::cout << "Fullscreen\n";
+  XEvent xev;
+  Atom wmState = XInternAtom(display, "_NET_WM_STATE", False);
+  Atom fullscreenAtom = XInternAtom(display, "_NET_WM_STATE_FULLSCREEN", False);
+  if (!fullscreenAtom)
+  {
+    std::cerr << "Failed to get _NET_WM_STATE_FULLSCREEN atom." << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  xev.type = ClientMessage;
+  xev.xclient.window = window;
+  xev.xclient.message_type = wmState;
+  xev.xclient.format = 32;
+  xev.xclient.data.l[0] = 1;
+  xev.xclient.data.l[1] = fullscreenAtom;
+  xev.xclient.data.l[2] = 0;
+
+  int result = XSendEvent(display, root, False,
+                          SubstructureRedirectMask | SubstructureNotifyMask,
+                          &xev);
+  if (!result)
+  {
+    std::cerr << "Failed to send fullscreen event." << std::endl;
+  }
+  else
+  {
+    std::cout << "Fullscreen event sent successfully." << std::endl;
+  }
+}
+
+void Application::render()
+{
+  XDrawLine(display, window, gc, 10, 60, 180, 20);
 }
 
 void Application::updateSizes(int windowWidth, int windowHeight)
@@ -35,7 +231,7 @@ void Application::updateSizes(int windowWidth, int windowHeight)
   // TODO: rerender everything
 }
 
-bool parseArguments(AppConfig &appConfig, int argc, char **argv)
+bool parseArguments(AppConfig &appConfig, int argc, char **argv, std::string &errorMessage)
 {
   int opt;
   while ((opt = getopt(argc, argv, "fFC:c:w:h:g")) != -1)
@@ -78,14 +274,14 @@ bool parseArguments(AppConfig &appConfig, int argc, char **argv)
   // Resolve mutually exclusive arguments
   if (appConfig.filePath.empty())
   {
-    std::cerr << "Error: You must specify either -c <path> or -C <path>\n\n";
+    errorMessage = "Error: You must specify either -c <path> or -C <path>\n\n";
     return false;
   }
 
   // Handle missing width/height when floating
   if (appConfig.floating && (appConfig.width == 0 || appConfig.height == 0))
   {
-    std::cerr << "Error: Floating mode requires both -w and -h options.\n\n";
+    errorMessage = "Error: Floating mode requires both -w and -h options.\n\n";
     return false;
   }
 
